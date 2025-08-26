782217fb73b45e2b85ccdf0b5bf297ab6b412de5
Elyor Xatanboyev <elxatamboyev2005@gmail.com>
2025-08-26 22:53:49 +0500

    .

--- obj/Debug/net9.0/Task.AssemblyInfo.cs
+++ obj/Debug/net9.0/Task.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc32b80f2a88d1d655986c203281a61da4de8670")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
